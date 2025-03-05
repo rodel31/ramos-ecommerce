a4a202d0ba435e3ba5cc590bbfa59fa6d94f5997
rodel31 <ramosrodel31lds@gmail.com>
2025-03-05 15:32:50 +0800

    add view page for products

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
             <iframe src="ramosLoginSignup.php" frameborder="0" class="iframe-loginsignup" scrolling="no"></iframe>
         </div>
         <iframe src="ramosMainPage.php" frameborder="0" class="iframe-main" name="mid-column"></iframe>
+        <iframe src="ramosview.php" frameborder="0" class="iframe-view" name="mid-view"></iframe>
         <iframe src="ramosFooter.php" frameborder="0" class="iframe-footer" scrolling="no"></iframe>
     </div>
 </body>
---
+++ ramosView.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Products</title>
+</head>
+<body class="view-page">
+    <div class="home-view">
+            <h1>Features</h1>
+        </div>
+    </body>
+</html>
